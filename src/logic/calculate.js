7209b2b86b65318f743279dc4ed02567ef7b697d
alexawesomecode <alexawesomecode@gmail.com>
2020-04-14 15:35:32 -0400

    added logic to calculator js

---
+++ src/logic/calculate.js
@@ -0,0 +1,74 @@
+const operate = require('./operate.js');
+
+const calculate = (data, button) => {
+  let { total } = data;
+  let prevOperation; let input; let
+    result;
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  const concatNumbers = arr => arr.reduce((acc, curr) => [acc, curr].join(''), '');
+
+  const lastOperation = oper => {
+    result = operate(total, concatNumbers(input), oper);
+    total = result;
+
+    return result;
+  };
+
+  const buttonName = button;
+  const currentOperation = () => {
+    if (numbers.includes(buttonName)) {
+      input.push(buttonName);
+    }
+
+    if (buttonName === '+') {
+      prevOperation = (typeof prevOperation === 'string') ? prevOperation : buttonName;
+      result = lastOperation(prevOperation);
+      total = result;
+      input = [];
+      prevOperation = '+';
+      return total;
+    }
+
+    if (buttonName === '-') {
+      prevOperation = (typeof prevOperation === 'string') ? prevOperation : buttonName;
+      result = lastOperation(prevOperation);
+      total = result;
+      input = [];
+      prevOperation = '-';
+      return total;
+    }
+    if (buttonName === 'x') {
+      prevOperation = (typeof prevOperation === 'string') ? prevOperation : buttonName;
+      result = lastOperation(prevOperation);
+      total = result;
+      input = [];
+      prevOperation = 'x';
+      return total;
+    }
+    if (buttonName === '%') {
+      prevOperation = (typeof prevOperation === 'string') ? prevOperation : buttonName;
+      result = lastOperation(prevOperation);
+      total = result;
+      input = [];
+      prevOperation = '%';
+      return total;
+    }
+    if (buttonName === '/') {
+      prevOperation = (typeof prevOperation === 'string') ? prevOperation : buttonName;
+      result = lastOperation(prevOperation);
+      total = result;
+      input = [];
+      prevOperation = '/';
+      return total;
+    }
+    if (buttonName === '=') return lastOperation(prevOperation);
+
+    return total;
+  };
+
+  return { lastOperation, concatNumbers, currentOperation };
+};
+
+
+export default calculate;
